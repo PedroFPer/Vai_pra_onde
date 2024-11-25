45297cd432d3c3494ed8af2ac0ab5ac39497422f
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2024-11-25 15:41:43 -0300

    Realizada ligação entre o android e o firebase

--- AndroidStudio/app/build.gradle.kts
+++ AndroidStudio/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")	 
+    id("com.google.gms.google-services")	 
 }
 
 android {
@@ -8,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ducks.vai_pra_onde"
-        minSdk = 16
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -40,4 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
