d225d096d5ab4c2a96333aad6e8469e9d61a2248
JohnWilliam24dev <163377183+JohnWilliam24dev@users.noreply.github.com>
2024-11-29 23:19:07 -0300

    EXECUSÃO DO PROJETO KTS.MORRA

--- AndroidStudio/app/src/main/java/com/ducks/vai_pra_onde/Geral/DAO/FirestoreDAO/FirestoreConnection.java
+++
@@ -1,9 +0,0 @@
-/*package com.ducks.vai_pra_onde.Geral.DAO.FirestoreDAO;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-
-public class FirestoreConnection {
-    public static FirebaseFirestore getConnection() {
-        return FirebaseFirestore.getInstance();
-    }
-}*/
