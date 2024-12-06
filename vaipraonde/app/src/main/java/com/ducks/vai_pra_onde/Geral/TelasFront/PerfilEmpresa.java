df2ebc848d3b83267a20263309192ac3e7702753
Daniel <166452209+star-Daniels@users.noreply.github.com>
2024-12-06 09:54:53 -0300

    img select

--- vaipraonde/app/src/main/java/com/ducks/vai_pra_onde/Geral/TelasFront/PerfilEmpresa.java
+++ vaipraonde/app/src/main/java/com/ducks/vai_pra_onde/Geral/TelasFront/PerfilEmpresa.java
@@ -1,16 +1,35 @@
 package com.ducks.vai_pra_onde.Geral.TelasFront;
 
+import static android.app.PendingIntent.getActivity;
+import static java.security.AccessController.getContext;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.ducks.vai_pra_onde.Geral.Utilidades.UtilArmazenImage;
 import com.ducks.vai_pra_onde.R;
 
+import java.io.IOException;
+
 public class PerfilEmpresa extends AppCompatActivity {
+
+    private ImageView imageViewSelected;
+    private  String saveImagePath;
+    private byte[] imagemData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +41,41 @@ public class PerfilEmpresa extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button buttonSelecIma = view.findViewById(R.id.bannerbtn);
+        imageViewSelect = view.findViewById(R.id.banner);
+
+        UtilArmazenImage utilArmIma = new UtilArmazenImage(getContext());
+
+        // ActivityResultLauncher para pegar a imagem
+        ActivityResultLauncher<Intent> imagePickerLaucher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == getActivity().RESULT_OK && result.getData() != null){
+                        Uri imagemUri = result.getData().getData();
+                        try {
+                            Bitmap bitMap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imagemUri);
+
+                            saveImagePath = utilArmIma.saveImage(bitMap);
+
+                            imagemData = utilArmIma.convertBitMapToByteArray(bitMap);
+
+                            viewModelPj.setDocumAutecEmpr(imagemData);
+
+                            imageViewSelect.setImageBitmap(bitMap);
+
+                            Toast.makeText(getContext(), "Imagem salva em " + saveImagePath, Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            Toast.makeText(getContext(), "Erro ao processar a imagem", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+        buttonSelecIma.setOnClickListener(v -> {
+            Intent intentUploImag = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            imagePickerLaucher.launch(intentUploImag);
+        });
+
     }
 }
