abfe7e68057713bcf09a22618845c4d44272119e
JohnWilliam24dev <163377183+JohnWilliam24dev@users.noreply.github.com>
2024-12-12 23:28:09 -0300

    tributo de hoje nessa budega

--- vaipraonde/app/src/main/java/com/ducks/vai_pra_onde/Geral/TelasFront/EventoCadastro.java
+++ vaipraonde/app/src/main/java/com/ducks/vai_pra_onde/Geral/TelasFront/EventoCadastro.java
@@ -1,6 +1,7 @@
 package com.ducks.vai_pra_onde.Geral.TelasFront;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,22 +12,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.ducks.vai_pra_onde.R;
 
-public class PerfilEvento extends AppCompatActivity {
-    TextView nome_evento=findViewById(R.id.nome_evento);
-    TextView nome_da_empresa=findViewById(R.id.nome_da_empresa);
-    TextView descrição=findViewById(R.id.descricao);
-    TextView data_evento=findViewById(R.id.data_evento);
-    TextView horario_evento=findViewById(R.id.horario_do_evento);
-    TextView endereco=findViewById(R.id.endereco);
+public class EventoCadastro extends AppCompatActivity {
+    TextView nome_evento=findViewById(R.id.nome_evento_cad);
+    TextView nome_da_empresa=findViewById(R.id.nome_da_empresa_cad);
+    TextView descricao =findViewById(R.id.descricao_cad);
+    TextView data_evento=findViewById(R.id.data_evento_cad);
+    TextView horario_evento=findViewById(R.id.horario_do_evento_cad);
+    TextView endereco=findViewById(R.id.endereco_cad);
+    Button submit=findViewById(R.id.BtnSubmit);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_perfil_evento);
+        setContentView(R.layout.activity_evento_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+
+
+
             return insets;
+        });
+        submit.setOnClickListener(v->{
+            
         });
     }
 }
