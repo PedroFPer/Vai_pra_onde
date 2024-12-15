b6173c6156f7f534a30c59991d064381259b8089
saydaires <saydaires@gmail.com>
2024-12-15 10:34:06 -0300

    Resolvido Problema: Atualização da Lista
    O mecanismo para a resolução da lista de Eventos atualizada em tempo real de aplicação foi implementado.

---
+++ vaipraonde/app/src/main/java/com/ducks/vai_pra_onde/Geral/novaSERVICE/FrontEndSERVICE/AtualizarListaSERVICE.java
@@ -0,0 +1,14 @@
+package com.ducks.vai_pra_onde.Geral.novaSERVICE.FrontEndSERVICE;
+
+import com.ducks.vai_pra_onde.Geral.DAO.Carregamentos.DownloadEventos;
+import com.ducks.vai_pra_onde.Geral.novaDTO.PessoaPJ;
+
+public class AtualizarListaSERVICE {
+    public static void atualizarLista(PessoaPJ pessoaLogada) {
+        DownloadEventos.carregarEventos(pessoaLogada).thenAccept(listaEventosAtualizada -> {
+            /* Chamar a tela "TelaInicial" e passar como parametros
+               o objeto "pessoaLogada" e "listaEventosAtualizada"
+             */
+        });
+    }
+}
